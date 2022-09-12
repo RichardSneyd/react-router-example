991e510e1aea1f860b6dd7abf351c6722789ccbb
RichardSneyd <richardsneyd@hotmail.com>
2022-09-12 12:49:12 +0100

    mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About </Link></li>
+          <li><Link to='/services'>Services</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
+        </ul>
+        {/* Switch is replaced by Routes in v6 */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
+function Home() {
+  return <><h2>Home Page</h2></>
+}
+
+function About() {
+  return <h2>About Page</h2>
+}
+
+function Services() {
+  return <h2>Services Page</h2>
+}
+
+function Contact() {
+  return <h2>Contact Page</h2>
+}
 export default App;
